69edfa619b8715733375b218d1e94629b0643171
Salsabil DOUHI <douhisalsabil@gmail.com>
2023-12-31 01:36:32 +0100

    conflit

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -2,8 +2,12 @@ package ma.ehei.calculatrice;
 
 public class App {
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		System.out.println(Calculatrice.add(5, 55));
 		System.out.println(Calculatrice.add(5, 35));
 		System.out.println(Calculatrice.add(5, 5));
+=======
+		System.out.println(Calculatrice.multiplication(5, 5));
+>>>>>>> 410e0fa (Problème de la fonction de multiplication a été résolu)
 	}
 }
--- src/main/java/ma/ehei/calculatrice/Calculatrice.java
+++ src/main/java/ma/ehei/calculatrice/Calculatrice.java
@@ -1,7 +1,12 @@
 package ma.ehei.calculatrice;
 
 public class Calculatrice {
+<<<<<<< HEAD
 	public static int add(int a, int b) {
 		return a+b;
+=======
+	public static int multiplication(int a, int b) {
+		return a*b;
+>>>>>>> 410e0fa (Problème de la fonction de multiplication a été résolu)
 	}
 }
